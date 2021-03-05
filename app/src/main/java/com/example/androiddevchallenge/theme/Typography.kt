f18c28ec823aeca445e1f72b3917a921b3e3251b
David Odari <davidkibzodari@gmail.com>
2021-03-05 23:25:31 +0100

    modify typography for the duration text

---
+++ app/src/main/java/com/example/androiddevchallenge/theme/Typography.kt
@@ -0,0 +1,23 @@
+package com.example.androiddevchallenge.theme
+
+import androidx.compose.material.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.example.androiddevchallenge.R
+
+val fontFamilies = FontFamily(
+    fonts = listOf(
+        Font(R.font.fredoka_one_regular)
+    )
+)
+
+val typography = Typography(
+    h3 = TextStyle(
+        fontFamily = fontFamilies,
+        fontWeight = FontWeight.Normal,
+        fontSize = 64.sp
+    )
+)
